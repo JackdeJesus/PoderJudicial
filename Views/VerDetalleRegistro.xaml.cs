05d56b48476a50525ff69c016e1ca31977710d64
Jack de Jesus <jackcornej1551@gmail.com>
2026-05-14 13:17:51 -0600

    .

---
+++ Views/VerDetalleRegistro.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PoderJudicial.Views
+{
+    public partial class VerDetalleRegistro : Window
+    {
+        public VerDetalleRegistro()
+        {
+            InitializeComponent();
+        }
+
+        public void CargarDatos(
+            string id, string noCausa, string nuc, string fechaAudiencia,
+            string fechaRecibo, string horaConclusion, string tipoAudiencia,
+            string tipoCausa, string juzgado, string juez, string sala,
+            string totalDiscos, string tipoDisco, string totalDiscoAudiencia,
+            string imputado, string delito, string agraviado,
+            string noCausaJuicio, string diferida, string quienRealiza)
+        {
+            TxtID.Text = id;
+            TxtNoCausa.Text = noCausa;
+            TxtNUC.Text = nuc;
+            TxtFechaAudiencia.Text = fechaAudiencia;
+            TxtFechaRecibo.Text = fechaRecibo;
+            TxtHoraConclusion.Text = horaConclusion;
+            TxtTipoAudiencia.Text = tipoAudiencia;
+            TxtTipoCausa.Text = tipoCausa;
+            TxtJuzgado.Text = juzgado;
+            TxtJuez.Text = juez;
+            TxtSala.Text = sala;
+            TxtTotalDiscos.Text = totalDiscos;
+            TxtTipoDisco.Text = tipoDisco;
+            TxtTotalDiscoAudiencia.Text = totalDiscoAudiencia;
+            TxtImputado.Text = imputado;
+            TxtDelito.Text = delito;
+            TxtAgraviado.Text = agraviado;
+            TxtNoCausaJuicio.Text = noCausaJuicio;
+            TxtDiferida.Text = diferida;
+            TxtQuienRealiza.Text = quienRealiza;
+        }
+
+        
+        // Permite arrastrar la ventana desde cualquier parte
+        protected override void OnMouseLeftButtonDown(
+            System.Windows.Input.MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+
+        private void BtnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
