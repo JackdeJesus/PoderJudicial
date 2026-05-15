e3734d2c5964702a44a6f94a5b78a146bc227b8e
Jack de Jesus <jackcornej1551@gmail.com>
2026-05-15 10:14:22 -0600

    Side bar dinamico

--- Views/Dashboard.xaml.cs
+++ Views/Dashboard.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using PoderJudicial.Views;
 
 namespace PoderJudicial.Views
 {
@@ -20,31 +20,80 @@ namespace PoderJudicial.Views
         public Dashboard()
         {
             InitializeComponent();
-            
-            MainFrame.Navigate(new PoderJudicial.Views.ConsultarRegistros());
 
+            // BOTÓN INICIAL ACTIVO
+            ActivarBoton(BtnConsultar);
+
+            // PÁGINA INICIAL
+            MainFrame.Navigate(new ConsultarRegistros());
         }
 
+        // ACTIVAR BOTÓN
+        private void ActivarBoton(Button botonActivo)
+        {
+            // TODOS LOS BOTONES
+            Button[] botones =
+            {
+                BtnConsultar,
+                BtnNuevo,
+                BtnReportes,
+                BtnConfig
+            };
+
+            // DESACTIVAR TODOS
+            foreach (Button btn in botones)
+            {
+                btn.Background =
+                    System.Windows.Media.Brushes.Transparent;
+
+                btn.Foreground =
+                    new System.Windows.Media.SolidColorBrush(
+                        (System.Windows.Media.Color)
+                        System.Windows.Media.ColorConverter.ConvertFromString("#8B92A5"));
+            }
+
+            // ACTIVAR SOLO UNO
+            botonActivo.Background =
+                new System.Windows.Media.SolidColorBrush(
+                    (System.Windows.Media.Color)
+                    System.Windows.Media.ColorConverter.ConvertFromString("#2A3147"));
+
+            botonActivo.Foreground =
+                new System.Windows.Media.SolidColorBrush(
+                    (System.Windows.Media.Color)
+                    System.Windows.Media.ColorConverter.ConvertFromString("#2ECC8F"));
+        }
+
+        // CONSULTAR
         private void BtnConsultar_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new PoderJudicial.Views.ConsultarRegistros());
+            ActivarBoton(BtnConsultar);
 
+            MainFrame.Navigate(new ConsultarRegistros());
         }
 
+        // NUEVO
         private void BtnNuevo_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new PoderJudicial.Views.NuevoRegistro());
+            ActivarBoton(BtnNuevo);
+
+            MainFrame.Navigate(new NuevoRegistro());
         }
 
+        // REPORTES
         private void BtnReportes_Click(object sender, RoutedEventArgs e)
         {
+            ActivarBoton(BtnReportes);
 
+            // MainFrame.Navigate(new Reportes());
         }
 
+        // CONFIG
         private void BtnConfig_Click(object sender, RoutedEventArgs e)
         {
+            ActivarBoton(BtnConfig);
 
+            // MainFrame.Navigate(new Configuracion());
         }
     }
 }
-
